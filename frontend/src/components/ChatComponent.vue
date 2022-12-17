2f78f1dbcb89cda9a47d8abde5a15beeb6e55309
Anton Semenikhin <protest_the_hero@mail.ru>
2022-12-17 18:00:56 +0300

    add infinity scroll, load messages on load

--- frontend/src/components/ChatComponent.vue
+++ frontend/src/components/ChatComponent.vue
@@ -1,14 +1,14 @@
 <template lang="pug">
-.q-pa-md.justify-between.column(style='width: 100%')
-  .overflow-auto.q-pb-lg
-    q-chat-message(
-      v-for='message in messagesItems',
-      :key='message._id',
-      :name='message.sender.name',
-      :text='[message.text]',
-      :sent='message.sender.name === "Anton10"'
-    )
-
+.justify-between.column(style='width: 100%', @load='scroll')
+  q-scroll-area.fit.q-px-lg.q-pb-xl(ref='chatScroll')
+    q-infinite-scroll(@load='onLoad', reverse)
+      q-chat-message(
+        v-for='message in messagesItems',
+        :key='message._id',
+        :name='message.sender.name',
+        :text='[message.text]',
+        :sent='message.sender.name === authStore.getUser.name'
+      )
   form.q-pa-md.fixed-bottom.bg-white(@submit.prevent='sendMessage')
     q-input.row.justify-end(v-model='messageData.text')
       q-btn(type='submit', label='Отправить', color='primary')
@@ -19,35 +19,67 @@ import { computed, onMounted, ref } from 'vue';
 import { useMessages } from 'src/stores/services/messages';
 import { useAuth } from 'src/stores/services/auth';
 
+// Stores
 const authStore = useAuth();
 const messagesStore = useMessages();
+//
 
+// Refs
 const messageData = ref({
   sender: authStore.getUser._id,
   text: '',
 });
-
+const messagesLimit = ref(15);
 const user = ref(messageData.value.sender);
 
-const messagesItems = computed(() => {
-  return messagesStore.findInStore({}).data;
-});
+const chatScroll = ref();
+//
 
+// Computed
+const messagesItems = computed(() => {
+  return messagesStore.findInStore({
+    query: {
+      $sort: {
+        createdAt: 1,
+      },
+    },
+  }).data;
+});
+//
+
+// Methods
 const sendMessage = async () => {
   const createMessage = messagesStore.Model;
   const message = new createMessage(messageData.value);
   const newMessage = await message.save();
 
   messageData.value.text = '';
+  scroll();
 };
 
-onMounted(() => {
-  messagesStore.find({
-    // query: { $populate: ['sender'] },
-    paginate: {
-      default: 100,
-      max: 200,
+const scroll = () => {
+  const { getScrollTarget, setScrollPosition } = chatScroll.value;
+  const { scrollHeight } = getScrollTarget();
+  setScrollPosition('vertical', scrollHeight, 0);
+};
+
+const onLoad = async (index, done) => {
+  await messagesStore.find({
+    query: {
+      $populate: ['sender'],
+      $sort: {
+        createdAt: -1,
+      },
+      $limit: messagesLimit.value,
+      paginate: {
+        default: 100,
+        max: 200,
+      },
     },
   });
-});
+  done();
+  messagesLimit.value += 5;
+
+  //
+};
 </script>
